6e7c6bc67680fb5a81b79aab0bc6ec2bed9b2ef7
YuriyAndreevich <yuriy.sys.apf@gmail.com>
2022-11-21 17:35:54 +0200

    hw7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const imgMarkup = createImgMarkup(galleryItems);
 
+galleryContainer.insertAdjacentHTML("beforeend", imgMarkup);
+
+function createImgMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+             />
+         </a>
+    
+    `;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  nav: true,
+  close: true,
+  caption: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  captionSpeed: 250,
+});
 console.log(galleryItems);
